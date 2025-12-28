c9b8308a6173b926b2478ef26d673416999b0dbf
Kurt Grutzmacher <grutz@802secure.com>
2025-12-28 00:54:05 -0800

    select hci device for discoverer

--- examples/discoverer.go
+++ examples/discoverer.go
@@ -6,9 +6,10 @@ package main
 import (
 	"fmt"
 	"log"
+	"os"
+	"strconv"
 
 	"github.com/grutz/gatt"
-	"github.com/grutz/gatt/constants"
 	"github.com/grutz/gatt/examples/option"
 )
 
@@ -17,7 +18,7 @@ func onStateChanged(d gatt.Device, s gatt.State) {
 	switch s {
 	case gatt.StatePoweredOn:
 		fmt.Println("scanning...")
-		d.Scan([]constants.UUID{}, false)
+		d.Scan(nil, false)
 		return
 	default:
 		d.StopScanning()
@@ -27,13 +28,27 @@ func onStateChanged(d gatt.Device, s gatt.State) {
 func onPeriphDiscovered(p gatt.Peripheral, a *gatt.Advertisement, rssi int) {
 	fmt.Printf("\nPeripheral ID:%s, NAME:(%s)\n", p.ID(), p.Name())
 	fmt.Println("  Local Name        =", a.LocalName)
-	fmt.Println("  TX Power Level    =", a.TxPowerLevel)
+	fmt.Println("  RSSI				 =", rssi)
 	fmt.Println("  Manufacturer Data =", a.ManufacturerData)
 	fmt.Println("  Service Data      =", a.ServiceData)
 }
 
 func main() {
-	d, err := gatt.NewDevice(option.DefaultClientOptions...)
+	// Get device id, 0 or 1, from the command line arguments
+	// and open device with that id
+	devIdStr := os.Args[1]
+	devId, err := strconv.Atoi(devIdStr)
+	if err != nil {
+		log.Fatalf("Invalid device id: %s\n", devIdStr)
+		return
+	}
+	fmt.Println("Using device id:", devId)
+
+	opt := option.DefaultClientOptions
+	opt = append(opt, gatt.LnxDeviceID(devId, false))
+	opt = append(opt, gatt.LnxSetScanMode(true)) // Passive scanning
+
+	d, err := gatt.NewDevice(opt...)
 	if err != nil {
 		log.Fatalf("Failed to open device, err: %s\n", err)
 		return
